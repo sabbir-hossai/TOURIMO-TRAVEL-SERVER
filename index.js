001603fb1d1a87567c768e00a697340abc951c4c
sabbir-hossai <sabbirab3@gmail.com>
2021-10-31 22:57:57 +0600

    send

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const ObjectId = require('mongodb').ObjectId;
 
 require('dotenv').config()
